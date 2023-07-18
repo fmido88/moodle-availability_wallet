02791eca989630b5379be70e6658961c45dec38c
Mo Farouk <122031508+fmido88@users.noreply.github.com>
2023-07-18 08:22:17 +0300

    Add files via upload

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023062923;
+$plugin->version = 2023071808;
 $plugin->requires = 2014111000;
 $plugin->component = 'availability_wallet';
-$plugin->release = 1.2;
+$plugin->release = 1.3;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'enrol_wallet' => 2023061002
+    'enrol_wallet' => 2023071808
 ];
